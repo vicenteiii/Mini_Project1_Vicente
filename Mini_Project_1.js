cc09a1038cf96e79b6fa9a0a32663ffb4b33bf5d
Vicente <vicente.hallasgo@deped.gov.ph>
2022-11-24 21:41:14 +0800

    initial commit

---
+++ Mini_Project_1.js
@@ -0,0 +1,8 @@
+var btn = document.getElementById("btn");
+var light = document.getElementById("show");
+var Pic = document.getElementById("Pic");
+    function toggleBtn(){
+        btn.classList.toggle("active")
+        light.classList.toggle("on")
+        Pic.classList.toggle("on")
+    }
